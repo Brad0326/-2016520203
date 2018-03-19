e0ec4e5b60a475895025e3b8b5bfb16b448cd11e
Brad0326 <wqs0601@163.com>
2018-03-19 18:15:04 +0800

    my work

---
+++ 1个人项目/计算生成器APP/app/src/main/java/com/wqs/0318/Question.java
@@ -0,0 +1,44 @@
+package com.wqs.0318;
+
+/**
+ * Created by hjy on 2018/3/13.
+ */
+
+public class Question {
+
+    String question;
+    double answer;
+    double userAnswer;
+
+    public Question() {
+    }
+
+    public Question(String question, double answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public double getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(double answer) {
+        this.answer = answer;
+    }
+
+    public double getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(double userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+}
---
+++ 1个人项目/计算生成器APP/app/src/main/java/com/wqs/0318/Tool.java
@@ -0,0 +1,36 @@
+package com.wqs.0318;
+
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by hjy on 2018/3/14.
+ */
+
+public class Tool {
+
+    public static double accuracy(List<Question> questions){
+
+        double a,b,m=0;
+        for (int i = 0; i < questions.size(); i++) {
+            a = questions.get(i).getAnswer();
+            b = questions.get(i).getUserAnswer();
+            Log.i("hahaha", "accuracy: "+ a + b);
+            if (Math.abs(a - b) < 0.000001) {
+                m ++;
+            }
+        }
+        return (m / questions.size()) * 100;
+    }
+
+    public static double decimalRandom(int numberRange,int decimalNumber){
+        double a,c;
+        a = (int)(Math.random() * numberRange);
+        c = (int)(Math.random() * Math.pow(10,decimalNumber));
+        c = c / Math.pow(10,decimalNumber);
+        a = a + c;
+        return a;
+    }
+
+}
